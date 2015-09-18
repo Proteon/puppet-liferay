09032fea21b1e923f4653d5ee4197d3651cb7a15
Freark van der Bos <safariman@gmail.com>
2015-09-18 14:31:42 +0200

    Allow setting a property for a particular portal instance by passing on it's web_id

--- manifests/property.pp
+++ manifests/property.pp
@@ -10,17 +10,43 @@
 #
 define liferay::property (
     $instance,
+    $web_id = undef,
     $type = 'portal',
     $key,
-    $value) {
+    $value
+) {
     include tomcat
     
     if !($type in ['portal', 'portlet', 'system']) {
         fail("The property type must be one of 'portal', 'portlet' or 'system', but was '${type}'")
     }
 
+    if ($web_id != undef) and ($type != 'portal') {
+        fail('When setting a property for a portal instance by passing a web_id, the type MUST be portal')
+    }
+
+    if ($web_id != undef) {
+        $_web_id = $web_id
+
+        ensure_resource('concat', "${tomcat::params::home}/${instance}/tomcat/lib/${type}-${_web_id}.properties", {
+            owner => $instance,
+            group => $instance,
+            mode  => '0750',
+            require => File["${tomcat::params::home}/${instance}/tomcat/lib"],
+        })
+
+        ensure_resource('concat::fragment', "${instance} ${type}-${_web_id}.properties header", {
+            target  => "${tomcat::params::home}/${instance}/tomcat/lib/${type}-${_web_id}.properties",
+            order   => 00,
+            content => "# Managed by puppet\n",
+        })
+
+    } else {
+        $_web_id = 'ext'
+    }
+
     concat::fragment { $name:
-        target  => "${tomcat::params::home}/${instance}/tomcat/lib/${type}-ext.properties",
+        target  => "${tomcat::params::home}/${instance}/tomcat/lib/${type}-${_web_id}.properties",
         order   => 01,
         content => "${key}=${value}\n",
         notify  => Tomcat::Service[$instance],
