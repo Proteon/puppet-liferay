4be7fa396f2eab8951c5c2765a73fa8f36e6c168
Sven Aggenbach <sven@proteon.nl>
2017-03-21 09:53:22 +0100

    updated the exec expression to update the fetched file on the filesystem if they changed in git

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -2,6 +2,6 @@
 class liferay::scripts (
 ){
     exec { '/usr/local/sbin/gitlab_fetch_folder -u root -g root -m 755 -o /usr/local/sbin minions/liferay/generic':
-        unless  =>  '/bin/ls -1 /usr/local/sbin|/bin/grep liferay_ > /dev/null 2>&1',
+        unless  =>  '/usr/local/sbin/gitlab_fetch_folder -d -u root -g root -m 755 -o /usr/local/sbin minions/liferay/generic > /dev/null 2>&1',
     }
 }
