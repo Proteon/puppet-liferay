c40c3719ed30e90b0975c196dcf7e347c78292ae
Sven Aggenbach <sven@proteon.nl>
2018-06-06 12:15:20 +0200

    added if to prevent double inclusion of Package python-mysql

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -2,8 +2,10 @@
 class liferay::scripts (
     $version = '6.2',
 ){
-    package { 'python-mysql.connector': # required for liferay_get_preferences
-        ensure => 'present',
+    if ! defined(Package['python-mysql.connector']) {
+        package { 'python-mysql.connector': # required for liferay_get_preferences
+            ensure => 'present',
+        }
     }
 
     exec { '/usr/local/sbin/gitlab_fetch_folder -u root -g root -m 755 -o /usr/local/sbin minions/liferay/generic':
