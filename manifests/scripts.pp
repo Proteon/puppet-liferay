83794ac227434c513890ffc0a8a3298470a60735
Alwyn Kik <alwyn@firelay.com>
2018-07-25 11:07:15 +0200

    Add liferay generic include to usr/local/lib/proteon

--- manifests/scripts.pp
+++ manifests/scripts.pp
@@ -12,9 +12,14 @@ class liferay::scripts (
         unless  =>  '/usr/local/sbin/gitlab_fetch_folder -d -u root -g root -m 755 -o /usr/local/sbin minions/liferay/generic > /dev/null 2>&1',
     }
 
+    exec { '/usr/local/sbin/gitlab_fetch_file -u root -g root -m 644 -o /usr/local/lib/proteon/liferay_generic_include.py libraries/liferay/generic/liferay_generic_include.py':
+        unless  =>  '/usr/local/sbin/gitlab_fetch_file -d -u root -g root -m 644 -o /usr/local/lib/proteon/liferay_generic_include.py libraries/liferay/generic/liferay_generic_include.py > /dev/null 2>&1',
+    }
+
     if versioncmp($version, '7.0') >= 0 {
         exec { '/usr/local/sbin/gitlab_fetch_folder -u root -g root -m 755 -o /usr/local/sbin minions/elastic/generic':
             unless  =>  '/usr/local/sbin/gitlab_fetch_folder -d -u root -g root -m 755 -o /usr/local/sbin minions/elastic/generic > /dev/null 2>&1',
         }
     }
 }
+
